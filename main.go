26a0557ae243d93040e2452e16fc4da2b8e9cd30
henron1 <40742091+henron1@users.noreply.github.com>
2023-01-25 15:23:57 -0500

    implement array/slice

--- main.go
+++ main.go
@@ -13,10 +13,13 @@ func main() {
 	fmt.Printf("We have a total of %v tickets and %v are avaliable.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend the lectures!")
 
+	bookings := []string{}
+
 	var firstName string
 	var lastName string
 	var email string
-	var userTickets int
+	var userTickets uint
+
 	// ask user for their name
 	fmt.Println("Enter your first name: ")
 	fmt.Scan(&firstName)
@@ -30,6 +33,11 @@ func main() {
 	fmt.Println("How many ticktets would you like to purchase: ")
 	fmt.Scan(&userTickets)
 
-	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	remainingTickets = remainingTickets - userTickets
+	bookings = append(bookings, firstName+" "+lastName)
 
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets are remaining for %v\n", remainingTickets, conferenceName)
+
+	fmt.Printf("These are all of our bookings: %v", bookings)
 }
