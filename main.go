6f5fc468267d8afe7779f9b666b3211dd3ea0742
henron1 <40742091+henron1@users.noreply.github.com>
2023-01-27 14:33:31 -0500

    implemented conditionals and loops and light validation. more validation to come

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	conferenceName := "Go Conference"
 	const conferenceTickets = 50
 	var remainingTickets uint = 50
+	noTicketsRemaining := remainingTickets == 0
+
+	bookings := []string{}
 
 	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
 
@@ -13,31 +19,49 @@ func main() {
 	fmt.Printf("We have a total of %v tickets and %v are avaliable.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend the lectures!")
 
-	bookings := []string{}
+	for remainingTickets > 0 && len(bookings) < 50 {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
+		// ask user for their name
+		fmt.Println("Enter your first name: ")
+		fmt.Scan(&firstName)
 
-	// ask user for their name
-	fmt.Println("Enter your first name: ")
-	fmt.Scan(&firstName)
+		fmt.Println("Enter your last name: ")
+		fmt.Scan(&lastName)
 
-	fmt.Println("Enter your last name: ")
-	fmt.Scan(&lastName)
+		fmt.Println("Enter your email address: ")
+		fmt.Scan(&email)
 
-	fmt.Println("Enter your email address: ")
-	fmt.Scan(&email)
+		fmt.Println("How many ticktets would you like to purchase: ")
+		fmt.Scan(&userTickets)
 
-	fmt.Println("How many ticktets would you like to purchase: ")
-	fmt.Scan(&userTickets)
+		if userTickets <= remainingTickets {
+			remainingTickets = remainingTickets - userTickets
+			bookings = append(bookings, firstName+" "+lastName)
 
-	remainingTickets = remainingTickets - userTickets
-	bookings = append(bookings, firstName+" "+lastName)
+			fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+			fmt.Printf("%v tickets are remaining for %v\n", remainingTickets, conferenceName)
 
-	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
-	fmt.Printf("%v tickets are remaining for %v\n", remainingTickets, conferenceName)
+			firstNames := []string{}
+			for _, fullName := range bookings {
+				var splitNames = strings.Fields(fullName)
+				firstNames = append(firstNames, splitNames[0])
+			}
 
-	fmt.Printf("These are all of our bookings: %v", bookings)
+			fmt.Printf("The first names of the the bookings are: %v \n", firstNames)
+
+			if noTicketsRemaining {
+				fmt.Println("The conference is sold out, please check back next year!")
+				break
+			}
+		} else {
+			fmt.Printf("We only have %v tickets remaining, so you can't book %v tickets\n", remainingTickets, userTickets)
+			// skips the below code and goes to next iteration in main loop
+			continue
+		}
+
+	}
 }
